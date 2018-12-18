681c53a078916589b6d03c60179f3dc6f8ce3192
xiaoyue8 <xiaoyue8@outlook.com>
2018-12-18 15:52:49 +0800

    上传表单创建

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,17 @@
 # Create your views here.
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response,HttpResponse
+from django import forms
 
+class UserForm(forms.Form):
+	username = forms.CharField()
+	headImg = forms.FileField()
 
 def register(req):
-	return render_to_response('register.html',{})
+	if req.method == "POST":
+		uf = UserForm(req.POST,req.FILES)
+		if uf.is_valid():
+			print uf.cleaned_data
+			return HttpResponse('ok')
+	else :
+		uf = UserForm()	
+	return render_to_response('register.html',{'uf':uf})
